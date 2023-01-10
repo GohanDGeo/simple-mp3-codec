142be9dc675defcb28fe066e43410e25866822a1
myusername <45694080+GohanDGeo@users.noreply.github.com>
2023-01-10 15:30:21 +0200

    Fixed roll

--- subband_filtering.py
+++ subband_filtering.py
@@ -6,47 +6,6 @@ import mp3
 import frame
 import nothing
 
-class InputBuffer:
-  
-    def __init__(self, size, type='float32'):
-        self.size = size
-        self.pos  = 0
-        self.samples = np.zeros(size, dtype=type)
-
-    def insert(self, data_in):
-        length = len(data_in)
-        if self.pos + length <= self.size:
-            self.samples[self.pos:self.pos+length] = data_in
-        else:
-            overhead = length - (self.size - self.pos)
-            self.samples[self.pos:self.size] = data_in[:-overhead]
-            self.samples[0:overhead] = data_in[-overhead:]
-        self.pos += length
-        self.pos %= self.size
-
-class InputBuffer2D:
-  
-    def __init__(self, size1, size2, type='float32'):
-        self.size1 = size1
-        self.size2 = size2
-        self.pos  = 0
-        self.samples = np.zeros((size1, size2), dtype=type)
-
-    def insert(self, data_in):
-        length = data_in.shape[0]
-
-        if self.pos + length <= self.size1:
-            self.samples[self.pos:self.pos+length, :] = data_in
-            #print(data_in.shape)
-            #print(self.samples[self.pos:self.pos+length, :].shape)
-        else:
-            overhead = length - (self.size1 - self.pos)
-            self.samples[self.pos:self.size1, :] = data_in[:-overhead, :]
-            self.samples[0:overhead, :] = data_in[-overhead:, :]
-
-        self.pos += length
-        self.pos %= self.size1
-
 def coder0(wavin, h, M, N):
 
     H  = mp3.make_mp3_analysisfb(h, M)
@@ -68,10 +27,11 @@ def coder0(wavin, h, M, N):
     buffer = np.zeros((N-1)*M + L)
     print(buffer.shape)
     i = 0
+    buffer[L-M:] = data_pad[:frame_size]
     # For each frame, read points from the file
-    for f in range(num_of_frames):
+    for f in range(1, num_of_frames):
 
-        buffer = np.roll(buffer, -(L-M))
+        buffer = np.roll(buffer, -(M*N))
         buffer[L-M:] = data_pad[f*frame_size:(f+1)*frame_size]
         # STEP (b)
         Y = frame.frame_sub_analysis(buffer, H, N)
@@ -100,7 +60,7 @@ def decoder0(Ytot, h, M, N):
 
         # STEP (e)
         Yh = nothing.idonothing(Yc)
-        buffer = np.roll(buffer, -int(-1 + L/M),axis=0)
+        buffer = np.roll(buffer, -N,axis=0)
         buffer[int(-1 + L/M):, :] = Yh
 
         # STEP (f)
