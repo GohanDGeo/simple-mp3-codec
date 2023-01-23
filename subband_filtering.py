6bab2c418d56562f0632ee010ce1ee796c9aebfb
myusername <45694080+GohanDGeo@users.noreply.github.com>
2023-01-23 10:53:43 +0200

    Type 2?

--- subband_filtering.py
+++ subband_filtering.py
@@ -33,7 +33,7 @@ def coder0(wavin, h, M, N):
         Y = frame.frame_sub_analysis(buffer, H, N)
         
         # STEP (c)
-        Yc = frameDCT(Y)
+        Yc = nothing.donothing(Y)
 
         # Step (d)5
         Ytot.append(Yc)
@@ -55,7 +55,7 @@ def decoder0(Ytot, h, M, N):
     for Yc in Ytot:
 
         # STEP (e)
-        Yh = iframeDCT(Yc)
+        Yh = nothing.idonothing(Yc)
         Yhtot.append(Yh)
     
     Yhtot = np.vstack(Yhtot)
